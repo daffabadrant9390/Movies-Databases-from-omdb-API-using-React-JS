18be558bb357c86b89a033edc7f86a23e9057168
M. Daffa Badran Thoriq <daffabadrant@gmail.com>
2021-12-28 10:20:30 +0700

    finish the search input

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -4,40 +4,63 @@ import { Link } from "react-router-dom";
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("marvel");
 
   useEffect(() => {
     getDataMovies();
     console.log(movies);
-  }, []);
+  }, [query]);
 
   const fetchDataMovies = () => {
-    return fetch("http://www.omdbapi.com/?apikey=7c36f70a&s=marvel")
+    return fetch(`http://www.omdbapi.com/?apikey=7c36f70a&s=${query}`)
       .then((response) => response.json())
       .then((response) => response);
   };
 
   const getDataMovies = async () => {
     const dataMovies = await fetchDataMovies();
-    console.log(dataMovies);
     setMovies(dataMovies.Search);
   };
 
+  const updateSearchInput = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const updateQuerySearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
+
   return (
-    <div className="movies-container">
-      {movies.map((movie) => {
-        return (
-          <div className="card-movies">
-            <img src={movie.Poster} alt={movie.Title} />
-            <div className="card-body">
-              <h5>{movie.Title}</h5>
-              <p>{movie.Year}</p>
-              <Link className="link-btn" to={`/movies/${movie.imdbID}`}>
-                See Details
-              </Link>
+    <div className="movies-section">
+      <form onSubmit={updateQuerySearch}>
+        <input
+          type="text"
+          className="input-search"
+          value={search}
+          onChange={updateSearchInput}
+        />
+        <button type="submit" className="btn-search">
+          Search
+        </button>
+      </form>
+      <div className="movies-container">
+        {movies.map((movie) => {
+          return (
+            <div className="card-movies">
+              <img src={movie.Poster} alt={movie.Title} />
+              <div className="card-body">
+                <h5>{movie.Title}</h5>
+                <p>{movie.Year}</p>
+                <Link className="link-btn" to={`/movies/${movie.imdbID}`}>
+                  See Details
+                </Link>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
